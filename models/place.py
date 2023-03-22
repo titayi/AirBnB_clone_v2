a381a1d73ecc6e8e89205e20f0a2a1c3c7ce8d1d
titayi <kitayimike@gmail.com>
2023-03-22 06:22:35 -0700

    File Updates

--- models/place.py
+++ models/place.py
@@ -5,9 +5,14 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
+association_table = Table('place_amenity', Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True,
+            nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
+
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -20,9 +25,23 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="all, delete")
-
+    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False, backref="amenities")
+    
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def reviews(self):
             """Returns the list of Review instances"""
             return [review for review in self.reviews]
+
+        @property
+        def amenities(self):
+            """Returns a list of Amenity instances"""
+            return [amenity for amenity in self.amenities]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Appends an amenity id to the attribute
+            amenity_id
+            """
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
