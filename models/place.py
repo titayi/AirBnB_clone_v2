63b7eb2f87310a82cb31583316635262796cfac8
titayi <kitayimike@gmail.com>
2023-03-23 00:48:14 -0700

    Updates

--- models/place.py
+++ models/place.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 association_table = Table('place_amenity', Base.metadata,
         Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
         Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True,
@@ -13,6 +12,7 @@ association_table = Table('place_amenity', Base.metadata,
 
 class Place(BaseModel, Base):
     """ A place to stay """
+
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -24,41 +24,24 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = [] 
     reviews = relationship("Review", backref="place", cascade="all, delete")
     amenities = relationship("Amenity", secondary='place_amenity', viewonly=False, backref="amenities")
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                               backref="place")
-
-        amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False,
-                                 back_populates="place_amenities")
-    else:
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def reviews(self):
-            """ Returns list of reviews.id """
-            var = models.storage.all()
-            lista = []
-            result = []
-            for key in var:
-                review = key.replace('.', ' ')
-                review = shlex.split(review)
-                if (review[0] == 'Review'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.place_id == self.id):
-                    result.append(elem)
-            return (result)
+            """Returns the list of Review instances"""
+            return [review for review in self.reviews]
 
         @property
         def amenities(self):
-            """ Returns list of amenity ids """
-            return self.amenity_ids
+            """Returns a list of Amenity instances"""
+            return [amenity for amenity in self.amenities]
 
         @amenities.setter
-        def amenities(self, obj=None):
-            """ Appends amenity ids to the attribute """
-            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+        def amenities(self, obj):
+            """Appends an amenity id to the attribute
+            amenity_id
+            """
+            if type(obj) == Amenity:
                 self.amenity_ids.append(obj.id)
