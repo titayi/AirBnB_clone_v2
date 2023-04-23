097fbd4de3c182c9f003292857a598adeadacbfc
titayi <kitayimike@gmail.com>
2023-04-23 14:54:21 +0000

    Task Solution

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Returns a string
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """
+    Returns a string
+    """
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    Returns a string
+    """
+    return ("C" + text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
