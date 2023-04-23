c8f6bd4f81d819fb23a837be001a1bf2e35ff16a
titayi <kitayimike@gmail.com>
2023-04-23 19:01:38 +0000

    Task Solution

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -87,3 +87,7 @@ class DBStorage():
             expire_on_commit=False
         )
         DBStorage.__session = scoped_session(session_factory)
+
+def close(self):
+        """closes the current db session"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+def close(self):
+        """calls reload method"""
+        self.reload()
